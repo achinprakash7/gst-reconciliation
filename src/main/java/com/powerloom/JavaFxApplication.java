cb8a5cc5b8f51192fa16fdb72dd5312972c27cd1
anant <anantprakash007@gmail.com>
2026-04-13 20:46:36 +0530

    GST Tools

---
+++ src/main/java/com/powerloom/JavaFxApplication.java
@@ -0,0 +1,30 @@
+package com.powerloom;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class JavaFxApplication extends Application {
+
+    private ConfigurableApplicationContext springContext;
+
+    @Override
+    public void init() {
+        springContext = new SpringApplicationBuilder(PowerloomApplication.class)
+                .headless(false)
+                .run(getParameters().getRaw().toArray(new String[0]));
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        springContext.publishEvent(new StageReadyEvent(primaryStage));
+    }
+
+    @Override
+    public void stop() {
+        springContext.close();
+        Platform.exit();
+    }
+}
