acffc008d3e92d21a8592d0e5c251d526afd7a76
anant <anantprakash007@gmail.com>
2026-04-17 19:40:34 +0530

    GST Tools

--- src/main/java/com/powerloom/ui/DarkThemeManager.java
+++ src/main/java/com/powerloom/ui/DarkThemeManager.java
@@ -4,19 +4,17 @@ import javafx.scene.Scene;
 
 public class DarkThemeManager {
 
-    private static final String LIGHT_CSS = "/css/app.css";
-    private static final String DARK_CSS  = "/css/dark.css";
-
+    private static final String DARK_CSS = "/css/dark.css";
     private boolean dark = false;
 
     public void toggle(Scene scene) {
+        String url = getClass().getResource(DARK_CSS).toExternalForm();
         if (dark) {
-            scene.getStylesheets().remove(
-                    getClass().getResource(DARK_CSS).toExternalForm());
+            scene.getStylesheets().remove(url);
             dark = false;
         } else {
-            scene.getStylesheets().add(
-                    getClass().getResource(DARK_CSS).toExternalForm());
+            if (!scene.getStylesheets().contains(url))
+                scene.getStylesheets().add(url);
             dark = true;
         }
     }
